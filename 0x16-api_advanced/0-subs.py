c5d4c088995bee9030fd809a8243df0013ce80bc
Koranteng Emmanuel <e.s.koranteng@gmail.com>
2024-08-15 08:36:25 -0700

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,9 +9,12 @@ def number_of_subscribers(subreddit):
     import requests
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "MyUserAgent"}
-    allow_redirects=False)
-    if sub_info.status_code >= 300:
+    headers = {
+            "User-Agent": "MyUserAgent"
+            }
+    response = requests.get(url, headers = headers, allow_redirects = False)
+    if response.status_code >= 300:
         return 0
 
-    return sub_info.json().get("data").get("subscribers")
+    results = response.json().get("data")
+    return results.get("subscribers")
